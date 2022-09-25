a51aff639f9aac3d8e629714f4e8f08e8822f3a7
sriranga <sriranga@Latitude-E7470>
2022-09-25 18:29:33 +0530

    new pdf added

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -13,6 +13,10 @@
 				<li class="mt-3"><a href="<?=BASE_URL?>describe/pdf/008" <?=($viewHelper->isLoggedIn())? 'target="_blank"' : '';?>><span class="english">A History Of South India</span></a></li>
 				<li class="mt-3"><a href="<?=BASE_URL?>describe/pdf/009" <?=($viewHelper->isLoggedIn())? 'target="_blank"' : '';?>><span class="english">Modern Mysore</span></a></li>
 				<li class="mt-3"><a href="<?=BASE_URL?>describe/pdf/010" <?=($viewHelper->isLoggedIn())? 'target="_blank"' : '';?>><span class="english">The Historical Record of The Imperial Visit To India-1911</span></a></li>
+
+				<li class="mt-3"><a href="<?=BASE_URL?>describe/pdf/011" <?=($viewHelper->isLoggedIn())? 'target="_blank"' : '';?>><span class="english">The Sri Krishnaraja Silver Jubilee Souvenir</span></a></li>
+
+
 			</ul>
 		</div>
 	</div>
